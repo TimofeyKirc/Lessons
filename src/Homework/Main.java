e9d5c33bc406cb94c011845d6f935fb5cf0517cc
TimofeyKirc <cringetop1102@gmail.com>
2023-09-28 21:12:01 +0300

    Homework1

---
+++ src/Homework/Main.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("Timofey");
+        }
+    }
+}
