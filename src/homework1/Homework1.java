cc67f7dd320eb2a4fccc1c422dc827dbc3671894
TimofeyKirc <cringetop1102@gmail.com>
2023-10-03 19:27:05 +0300

    Homework1

---
+++ src/homework1/Homework1.java
@@ -0,0 +1,10 @@
+package homework1;
+
+import java.util.Random;
+public class Homework1   {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int random_number = random.nextInt(20) -20;
+        System.out.println(random_number);
+    }
+}
